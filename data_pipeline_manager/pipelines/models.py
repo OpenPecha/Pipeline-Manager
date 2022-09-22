2f6df93c39a7b29f6745551e613aa2bb3fbb2b93
10zinten <ten13zin@gmail.com>
2022-09-22 16:58:28 +0530

    add Task model to track pipeline task

--- data_pipeline_manager/pipelines/models.py
+++ data_pipeline_manager/pipelines/models.py
@@ -1,3 +1,43 @@
-from django.db import models
+from xml.etree.ElementTree import PI
 
-# Create your models here.
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+class PipelineTypes(models.TextChoices):
+    IMPORT = "I", _("Import")
+    REIMPORT = "R", _("Reimport")
+
+class TaskStatus(models.TextChoices):
+    PENDING = "P", _("Pending")
+    RUNNING = "R", _("Running")
+    SUCCESS = "S", _("Success")
+    FAILURE = "F", _("Failure")
+
+
+class Task(models.Model):
+    task_id = models.CharField(max_length=32)
+    name = models.CharField(max_length=255)
+    started_on = models.DateTimeField(auto_now_add=True)
+    pipeline_type = models.CharField(
+        max_length=1, choices=PipelineTypes.choices, default=PipelineTypes.IMPORT
+    )
+    pipeline_config = models.JSONField()
+    completed_on = models.DateTimeField(null=True, blank=True)
+    result = models.CharField(max_length=32, null=True, blank=True)
+    error = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    @property
+    def status(self):
+        if self.error:
+            return TaskStatus.FAILURE
+        if self.completed_on:
+            return TaskStatus.SUCCESS
+        return TaskStatus.RUNNING
+
+    @property
+    def is_completed(self):
+        return self.completed_on is not None
