5e1717753c77beab996486a2b9a7aa23026113b5
10zinten <ten13zin@gmail.com>
2022-09-22 16:09:26 +0530

    add pipelines app

--- config/settings/base.py
+++ config/settings/base.py
@@ -80,7 +80,7 @@ THIRD_PARTY_APPS = [
 
 LOCAL_APPS = [
     "data_pipeline_manager.users",
-    # Your stuff: custom apps go here
+    "data_pipeline_manager.pipelines",
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
