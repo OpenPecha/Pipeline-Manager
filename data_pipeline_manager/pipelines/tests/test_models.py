1a4bb73501b565b70db3bbe88a3ca29229cb2b60
10zinten <ten13zin@gmail.com>
2022-10-03 12:09:00 +0530

    tests(pipelines): add BatchTask run test

--- data_pipeline_manager/pipelines/tests/test_models.py
+++ data_pipeline_manager/pipelines/tests/test_models.py
@@ -11,6 +11,18 @@ class TestBatchTask:
 
         assert batch is not None
 
+    def test_get_inputs(self):
+        batch = BatchFactory(inputs="a\nb\nc")
+
+        assert batch.get_inputs() == ["a", "b", "c"]
+
+    def test_run(self):
+        batch = BatchFactory(inputs="a\nb\nc")
+
+        batch.run()
+
+        assert batch.task_set.count() == 3
+
 
 class TestTask:
     def test_factory(self):
