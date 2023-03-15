9c10df1b1f82fdf1808e2cc284d084ce5080c1dc
10zinten <ten13zin@gmail.com>
2023-03-15 16:56:20 +0530

    add github webhook view

--- data_pipeline_manager/github_bot/views.py
+++ data_pipeline_manager/github_bot/views.py
@@ -1,3 +1,15 @@
-# from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+
+class GitHubWebhookView(APIView):
+    def post(self, request):
+        event_type = request.META.get("HTTP_X_GITHUB_EVENT")
+        payload = request.data
+        if event_type == "push":
+            print(payload)
+            return Response({"success": True})
+        else:
+            return Response(
+                {"success": False, "message": f"Invalid event type: {event_type}"}
+            )
