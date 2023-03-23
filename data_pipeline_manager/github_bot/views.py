aebe7ca25aada4f12357f0f2fab30367e5448fe8
10zinten <ten13zin@gmail.com>
2023-03-23 13:14:03 +0530

    fix(github-bot): valid issue action

--- data_pipeline_manager/github_bot/views.py
+++ data_pipeline_manager/github_bot/views.py
@@ -5,18 +5,21 @@ from rest_framework.views import APIView
 
 def notify_monlamAI_tracker(payload):
     if payload["action"] != "closed":
+        print("not closed issue")
         return
 
     issue = payload["issue"]
     repo = payload["repository"]
 
-    if not issue["title"].startswith("EN") or not issue["title"].startswith("BO"):
+    if not issue["title"].startswith("EN") and not issue["title"].startswith("BO"):
+        print("not EN or BO issue", issue["title"])
         return
 
     if not issue["title"] == repo["name"]:
+        print("not same repo name", issue["title"], repo["name"])
         return
 
-    return Response({"success": True, "message": "Tracker notified"})
+    return True
 
 
 class GitHubWebhookView(APIView):
@@ -27,7 +30,16 @@ class GitHubWebhookView(APIView):
         event_type = request.META.get("HTTP_X_GITHUB_EVENT")
         payload = request.data
         if event_type == "issues":
-            notify_monlamAI_tracker(payload)
+            created = notify_monlamAI_tracker(payload)
+            if created:
+                return Response({"success": True})
+            else:
+                return Response(
+                    {
+                        "success": False,
+                        "message": f"Invalid issue action: {payload['action']}",
+                    }
+                )
         else:
             return Response(
                 {"success": False, "message": f"Invalid event type: {event_type}"}
