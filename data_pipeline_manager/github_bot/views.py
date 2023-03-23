e7e632e38d3a90d92fa9a07f1d5889785030900e
10zinten <ten13zin@gmail.com>
2023-03-23 12:51:30 +0530

    feat(github-bot): github webhook listen for closed issue

--- data_pipeline_manager/github_bot/views.py
+++ data_pipeline_manager/github_bot/views.py
@@ -1,14 +1,33 @@
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 
+def notify_monlamAI_tracker(payload):
+    if payload["action"] != "closed":
+        return
+
+    issue = payload["issue"]
+    repo = payload["repository"]
+
+    if not issue["title"].startswith("EN") or not issue["title"].startswith("BO"):
+        return
+
+    if not issue["title"] == repo["name"]:
+        return
+
+    return Response({"success": True, "message": "Tracker notified"})
+
+
 class GitHubWebhookView(APIView):
+    authentication_classes = []
+    permission_classes = [AllowAny]
+
     def post(self, request):
         event_type = request.META.get("HTTP_X_GITHUB_EVENT")
         payload = request.data
-        if event_type == "push":
-            print(payload)
-            return Response({"success": True})
+        if event_type == "issues":
+            notify_monlamAI_tracker(payload)
         else:
             return Response(
                 {"success": False, "message": f"Invalid event type: {event_type}"}
